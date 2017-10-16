9356d47b3aed55e28780175e3b2623d3cc102715
JoeHe <hejinshan89@gmail.com>
2017-10-16 21:28:52 +0800

    Create log_helper.py

---
+++ jh/utils/log_helper.py
@@ -0,0 +1,40 @@
+import os, sys
+import logging
+
+def logHelper(fileName, logLevel=logging.INFO, useConsole=True):
+    """
+    Simple Logging Helper. Retuens logger reference.
+
+    Paramsmeters:
+    fileName: Filename, may include full path, or will open a file in default folder
+    logLevel: Pass logging.INFO, logging.DEBUG or other enums for logging level
+    useConsole: If Ture, will also dump log to console
+    """
+
+    ##### init logging
+    log = logging.getLogger()
+    log.setLevel(logLevel)
+    logFormatter = logging.Formatter("%(asctime)s | %(threadName)-12.12s | %(levelname)-5.5s | %(message)s")
+
+    ##### file handler
+    fileOut = logging.FileHandler(fileName)
+    fileOut.setFormatter(logFormatter)
+    log.addHandler(fileOut)
+
+    ##### console handler
+    if useConsole:
+        consoleOut = logging.StreamHandler(sys.stdout)
+        consoleOut.setFormatter(logFormatter)
+        log.addHandler(consoleOut)
+
+    ##### return reference
+    return log
+    
+
+# usage
+“”“
+import log_helper as log
+
+log = log.logHelper('logfile.log', logging.WARN, False)
+log.warn("this is a warning message")
+“”“
